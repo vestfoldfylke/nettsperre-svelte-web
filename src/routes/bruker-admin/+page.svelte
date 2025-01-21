fbefcdd6840ad09cd3782ce4d96ec5b4a8033417
Robin Ellingsen <robin-elli@hotmail.com>
2025-01-21 09:10:36 +0100

    feat: Add superUserImposterSessionID store and update validatePermission to include sessionId; generate GUID for session management in bruker-admin page

--- src/routes/bruker-admin/+page.svelte
+++ src/routes/bruker-admin/+page.svelte
@@ -2,7 +2,7 @@
     import IconSpinner from "../../lib/components/IconSpinner.svelte";
     import { getNettsperreToken, getGroupMembers, validatePermission } from "../../lib/useApi.js";
     import { onMount } from "svelte";
-    import { superUserImposter, teachersStore } from "../../lib/store"
+    import { superUserImposter, teachersStore, superUserImposterSessionID } from "../../lib/store"
     import { get } from "svelte/store";
     import { goto } from '$app/navigation'
 
@@ -49,9 +49,21 @@
 		teachers = groupMembersArray.filter(filterFunc)
 	}
 
+    const generateGUID = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            // Replace x or y with random hex digit
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            // Return the random guid like string
+            return v.toString(16);
+        });
+    }
+
     const setAdmin = async (requestor, teacher) => {
         processing = true
-        const permission = await validatePermission(requestor, teacher.userPrincipalName)
+        // Create a random session id (guid) for the user
+        const sessionId = generateGUID()
+        superUserImposterSessionID.set(sessionId)
+        const permission = await validatePermission(requestor, teacher.userPrincipalName, sessionId)
         if(permission.status === 200) {
             superUserImposter.set({
                 requestor: requestor,
@@ -132,6 +144,7 @@
                                 <h1>Hei, {token.name}!</h1>
                                 {#if get(superUserImposter).length !== 0}
                                     <h3 style="color: red;">Du er logget inn som: {get(superUserImposter).teacher.userPrincipalName}</h3>
+                                    <h3 style="color: red;">SessionId: {get(superUserImposterSessionID)}</h3>
                                     <button on:click={() => resetImposter()}>Fjern administrering</button>
                                 {/if}   
                             </div>
